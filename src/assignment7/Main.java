264437c8ac5e928d7d9cead1aed5d5c3aba701dd
eric-dailey <eric.dailey@utexas.edu>
2017-04-25 23:20:52 -0500

    test

---
+++ src/assignment7/Main.java
@@ -0,0 +1,42 @@
+package assignment7;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	
+	private Stage primaryStage;
+	private AnchorPane mainLayout;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Chat");
+		try {
+			showMainView();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void showMainView() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("view/ChatGUI.fxml"));
+		mainLayout = loader.load();
+		Scene scene = new Scene(mainLayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/assignment7/view/Chat.java
@@ -0,0 +1,42 @@
+package assignment7.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+
+public class Chat {
+
+	
+    @FXML
+    private AnchorPane mainpane;
+
+    @FXML
+    private TabPane tabpane;
+
+    @FXML
+    private Tab tab1;
+
+    @FXML
+    private AnchorPane anchor1;
+
+    @FXML
+    private SplitPane split1;
+
+    @FXML
+    private AnchorPane anchor1a;
+
+    @FXML
+    private TextArea textarea;
+
+    @FXML
+    private AnchorPane anchor1b;
+
+    @FXML
+	private void initialize(){
+    	
+    }
+    
+}
